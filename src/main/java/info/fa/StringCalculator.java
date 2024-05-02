6ee3f8f8c500af2504e72f629120da2ab909d9c6
frakhlystov <fr-alexander.khlystov@ajaib.co.id>
2024-05-02 20:54:41 +0800

    end of task

--- src/main/java/info/fa/StringCalculator.java
+++ src/main/java/info/fa/StringCalculator.java
@@ -2,21 +2,61 @@ package info.fa;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
-    public int add(String ... numbersAsString) {
-        if (numbersAsString.length == 1) {
-            if (StringUtils.isBlank(numbersAsString[0])) {
+    public int add(String ... input) {
+        List<Integer> negativeNumbers = new ArrayList<>();
+        if (input.length == 1) {
+            if (StringUtils.isBlank(input[0])) {
                 return 0;
             }
         }
-        int sum = Arrays.stream(numbersAsString)
-                .map(numberAsString -> Arrays.asList(numberAsString.replaceAll("\\s","").split(",")))
-                .flatMap(Collection::stream).mapToInt(Integer::parseInt).sum();
 
+        List<String> arraysOfNumbers = Arrays.stream(input)
+                .map(arrayOfStrings -> {
+                    List<String> delimiters = Arrays.asList(",");
+                    if (arrayOfStrings.startsWith("//")) {
+                        int newLineIndex = arrayOfStrings.indexOf("\n");
+                        String delimitersString = arrayOfStrings.substring(2, newLineIndex);
+                        arrayOfStrings = arrayOfStrings.substring(newLineIndex);
+                        delimiters = this.getDelimiters(delimitersString);
+                    }
+                    String delimiterRegexp = getDelimiterRegexp(delimiters);
+                    return Arrays.asList(arrayOfStrings.replaceAll("\\s", "").split(delimiterRegexp));
+                })
+                .flatMap(Collection::stream).collect(Collectors.toList());
+        int sum = arraysOfNumbers.stream().mapToInt(Integer::parseInt).filter(value -> {return value < 1001;}).sum();
+        arraysOfNumbers.forEach(value -> {
+            int anInt = Integer.parseInt(value);
+            if ( anInt < 0 ) {
+                negativeNumbers.add(anInt);
+            }
+        });
+        if (negativeNumbers.size() > 0) {
+            throw new RuntimeException("Negatives not allowed: " + negativeNumbers);
+        }
         return sum;
+    }
+
+    String getDelimiterRegexp(List<String> delimiters) {
+        if (delimiters.size() == 1) {
+            return delimiters.get(0);
+        }
+        return StringUtils.join(delimiters.stream().map(del -> "("+del + ")").collect(Collectors.toList()), "|");
+    }
+
+    List<String> getDelimiters(String delimitersString) {
+        List<String> list = Arrays.asList(delimitersString);
+        if (delimitersString.startsWith("[") && delimitersString.endsWith("]")) {
+            delimitersString = delimitersString.substring(1, delimitersString.length()-1);
+            list = Arrays.asList(delimitersString.split("]\\["));
+        }
+        return list;
     }
 }
--- src/test/java/info/fa/StringCalculatorTest.java
+++ src/test/java/info/fa/StringCalculatorTest.java
@@ -2,6 +2,9 @@ package info.fa;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -19,19 +22,87 @@ class StringCalculatorTest {
         assertEquals(1, calculator.add("1"));
     }
 
+    //1. Create a StringCalculator with a method Add(string numbers) that returns an integer.
     @Test
     void testAdd2numbers() {
         assertEquals(5, calculator.add("1", "4"));
     }
 
+    //2. Allow the Add method to handle an unknown number of numbers (in the string).
     @Test
     void testAddComplexNumbers() {
         assertEquals(10, calculator.add("1", "4,5"));
     }
 
+    //3. Allow the Add method to handle new lines between numbers (as well as commas):
     @Test
     void testAddComplexNumbersWhitespaces() {
-        assertEquals(5, calculator.add("1", "4\\n,5"));
+        assertEquals(10, calculator.add("1", "4\n,5"));
+    }
+
+    //4. Allow the Add method to handle a different delimiter:
+    @Test
+    void testCustomDelimiter() {
+        assertEquals(31, calculator.add("//;\n4;6", "//s\n4s6s10", "1"));
+    }
+
+    //5. Calling Add with a negative number will throw an exception "Negatives not allowed: " and then listing all negative numbers that were in the list of numbers.
+    @Test
+    void testNegativeNumber() {
+        RuntimeException thrown = assertThrows(
+                RuntimeException.class,
+                () -> calculator.add("//;\n4;6", "//s\n4s6s10", "-1", "-3,4,-5"),
+                "Expected doThing() to throw, but it didn't"
+        );
+        assertEquals("Negatives not allowed: [-1, -3, -5]", thrown.getMessage());
+    }
+
+    //6. Numbers greater than 1000 should be ignored.
+    @Test
+    void test1000plus() {
+        assertEquals(2030, calculator.add("//;\n4;6", "//s\n4s6s10", "1001, 1000, 999", "1"));
+    }
+
+    //7. Delimiters can be any length, using this syntax: "//[|||]\n1|||2|||3" returns 6.
+    @Test
+    void testLongDelimiter() {
+        assertEquals(31, calculator.add("//[;;;]\n4;;;6", "//s\n4s6s10", "1"));
+    }
+
+    //8. Allow multiple delimiters, using this syntax: "//[|][%]\n1|2%3" returns 6.
+    //9. Handle multiple delimiters of any length.
+    @Test
+    void testLongDelimiters() {
+        assertEquals(10, calculator.add("//[;;;][,,]\n1;;;2,,3", "4"));
+    }
+
+
+    //POC tests - not for PROD
+    @Test
+    void stringSplit() {
+        List result = Arrays.asList("1;;;2,,3".split("(;;;)|(,,)"));
+        assertEquals("[1, 2, 3]", result.toString());
+    }
+
+    @Test
+    void getOneDelimiters() {
+        List<String> delimiters = calculator.getDelimiters(";");
+        assertEquals(Arrays.asList(";"), delimiters);
+        assertEquals(";",calculator.getDelimiterRegexp(delimiters));
+    }
+
+    @Test
+    void getDelimiterInBrackets() {
+        List<String> delimiters = calculator.getDelimiters("[;]");
+        assertEquals(Arrays.asList(";"), delimiters);
+        assertEquals(";",calculator.getDelimiterRegexp(delimiters));
+    }
+
+    @Test
+    void get2DelimitersInBrackets() {
+        List<String> delimiters = calculator.getDelimiters("[;;][,]");
+        assertEquals(Arrays.asList(";;", ","), delimiters);
+        assertEquals("(;;)|(,)",calculator.getDelimiterRegexp(delimiters));
     }
 
 }
